4f56b3069689afff4da24e74b8774bddf878a704
reidsana <oksanareid48@gmail.com>
2025-11-19 13:47:01 -0500

    update

--- week3/scripts/place.js
+++ week3/scripts/place.js
@@ -3,22 +3,19 @@ document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("modified").textContent = document.lastModified;
 
 
+
 const temp = parseFloat(document.getElementById("temp").textContent);
 const wind = parseFloat(document.getElementById("wind").textContent);
 
-function calculateWindChill(T, W) {
-  return (
-    13.12 +
-    0.6215 * T -
-    11.37 * Math.pow(W, 0.16) +
-    0.3965 * T * Math.pow(W, 0.16)
-  );
+
+
+function calculateWindChill(T, W) { return 13.12 + 0.6215 * T - 11.37 * Math.pow(W, 0.16) + 0.3965 * T * Math.pow(W, 0.16); }
+
+
+let result = "N/A";
+if (TempCheck = (temp <= 10) && (wind > 4.8)) {
+result = calculateWindChill(temp, wind).toFixed(1) + " °C";
 }
 
-let output = "N/A";
 
-if (temp <= 10 && wind > 4.8) {
-  output = calculateWindChill(temp, wind).toFixed(1) + " °C";
-}
-
-document.getElementById("windchill").textContent = output;
+document.getElementById("windchill").textContent = result;
